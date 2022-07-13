80b350d9086be7b40cb8720a58bd6aafcfc01002
Suman Shakya <shakyasuman007@gmail.com>
2022-07-13 12:58:20 +0545

    update

--- script.js
+++ script.js
@@ -2,15 +2,31 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
+            
         }else{
             $('.navbar').removeClass("sticky");
+            
         }
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass('show');
         }else{
             $('.scroll-up-btn').removeClass('show');
         }
+        if(this.scrollY > 200){
+            $('.social').addClass('show');
+        }else{
+            $('.social').removeClass('show');}
     });
+
+    // loader animation with gsap
+    const tl = gsap.timeline({defaults:{ ease: "power1.out" }}); 
+    tl.to('.text',{y:'0%', duration: 1});
+    tl.to('.slider',{y:'-100%',duration:1, delay:0.5 });
+    tl.to('.intro',{y:'-100%', duration: 1}, '-=1'); /* -=1 means start 1 second earlier, same time as above code of slider */
+    tl.fromTo('nav',{opacity: 0},{opacity: 1, duration: 1.2});
+    tl.fromTo('.home-content',{opacity: 0},{opacity: 1, duration: 1.2}, '-=1');
+
+   
 
     // slide up script
     $('.scroll-up-btn').click(function(){
@@ -25,14 +41,14 @@ $(document).ready(function(){
 
     //typing animation script 
     var typed = new Typed('.typing',{
-        strings: ['Freelancer', 'Developer', 'CV Designer'],
+        strings: ['Freelancer', 'Web Developer', 'CV Designer'],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed('.typing-2',{
-        strings: ['Freelancer', 'Developer', 'CV Designer'],
+        strings: ['Freelancer', 'Web Developer', 'CV Designer'],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
